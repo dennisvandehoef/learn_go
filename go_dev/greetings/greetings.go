fb7b0a5cc4b68f3ef0012ec05cfb962aec4cfcf6
Dennis van de Hoef <dennis.vandehoef@aroundhome.de>
2022-02-03 07:43:25 +0100

    Return greetings for multiple people

--- go_dev/greetings/greetings.go
+++ go_dev/greetings/greetings.go
@@ -19,6 +19,23 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that accoiciates each og the named people with a
+// greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slices of names, calling the Hello function
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // init sets initial values for variables used in the function.
 func init() {
 	rand.Seed(time.Now().UnixNano())
--- go_dev/hello/hello.go
+++ go_dev/hello/hello.go
@@ -14,8 +14,10 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	names := []string{"Sarah", "Alek", "Dennis"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Dennis")
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
